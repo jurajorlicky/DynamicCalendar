68bc26b367cc6afe000ca014ba569401d3b2a840
Joren Meynen <joren.meynen@telenet.be>
2019-03-23 13:04:04 +0100

    Patient information

---
+++ DynamicCalendar/Patient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicCalendar
+{
+    public class Patient
+    {
+        public int id { get; set; }
+        public object remote_id { get; set; }
+        public string last_name { get; set; }
+        public string first_name { get; set; }
+        public string email { get; set; }
+        public string phone_number { get; set; }
+        public object birthdate { get; set; }
+        public string address { get; set; }
+        public string notes { get; set; }
+        public string status { get; set; }
+        public object metadata { get; set; }
+        public string language_code { get; set; }
+    }
+
+    public class PatientRootObject
+    {
+        public Patient patient { get; set; }
+    }
+}
---
+++ DynamicCalendar/PatientInformation.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicCalendar
+{
+    public class PatientInformation
+    {
+        public static void MakeFile(Appointment appointment, int center_id, string user_email, string user_token, int calendar_id)
+        {
+            string url = "https://" + "progenda.be/api/v2/centers/" + center_id + "/patients/" + appointment.appointment.patient_id + "?user_email=" + user_email + "&user_token=" + user_token;
+            WebClient webClient = new WebClient();
+            webClient.Headers.Add("User-Agent", "DynamicCalendar");
+            string downloadString = webClient.DownloadString(url);
+            PatientRootObject patientRootObject = JsonConvert.DeserializeObject<PatientRootObject>(downloadString);
+            
+            string patientString = "";
+            patientString += "First name: " + patientRootObject.patient.first_name + "\r\n";
+            patientString += "Last name: " + patientRootObject.patient.last_name + "\r\n";
+            patientString += "Email: " + patientRootObject.patient.email + "\r\n";
+            patientString += "Phone number: " + patientRootObject.patient.phone_number + "\r\n";
+            patientString += "Notes: " + appointment.appointment.notes;
+
+            WriteToFile(patientString);
+        }
+
+        public static void WriteToFile(string patientString)
+        {
+            FileIOPermission f = new FileIOPermission(PermissionState.None);
+            f.AllLocalFiles = FileIOPermissionAccess.Write;
+
+            string path =  Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Cancellations";
+            // This text is added only once to the file.
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            path += "\\" + DateTime.Today.ToString("yyyy - MM - dd") + ".txt";
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("**********************");
+                    sw.WriteLine(patientString);
+                    sw.WriteLine("**********************\r\n");
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine("**********************");
+                    sw.WriteLine(patientString);
+                    sw.WriteLine("**********************\r\n");
+                }
+            }
+        }
+    }
+}
